855aa0bcc270d1c1ef484b78023fb3a33c166dca
Adarsh Kumar Shrivastava <adarshshrivastava.dev@gmail.com>
2023-10-13 16:13:13 +0530

    I solved a error

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0eAO-jSySVPGPUOjldKKB5-d-iw81MVE",
-  authDomain: "clone-f2171.firebaseapp.com",
-  projectId: "clone-f2171",
-  storageBucket: "clone-f2171.appspot.com",
-  messagingSenderId: "596514836684",
-  appId: "1:596514836684:web:7b350a4bfaeaca51643269"
+  apiKey: "AIzaSyAVIQkE-CQ40hye0Zmomt2VriVjAHRsfBo",
+  authDomain: "clone-cc937.firebaseapp.com",
+  projectId: "clone-cc937",
+  storageBucket: "clone-cc937.appspot.com",
+  messagingSenderId: "938060480870",
+  appId: "1:938060480870:web:4f242625081bd1c74608a7"
 };
 
 const app = initializeApp(firebaseConfig);
